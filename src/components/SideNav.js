501c43e8d72f79053759baf167e5be0399d59825
Eric Wiseman <ewiseman1.com>
2021-02-19 21:30:16 -0700

    updated again

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -12,31 +12,53 @@ const SideNav = () => {
   const NavLinkAnimation = (props) => {
     if (!isNavCollapsed) {
       return  (
-        <Animated animationIn="fadeInLeft" animationInDuration={1000} animationInDelay={0} isVisible={true}>
-          <li className="fancynav-item fancy-dropdown"><a className="fancynav-link" href="/#"><span className="fancynav-link-content">Home </span></a>
-            <div className="fancy-dropdown-menu">
-              <div className="row pb-4 pt-3">
-                <div className="col-auto">
-                  <a className="fancy-dropdown-item" href="home-default.html">Default</a>
+        <Animated animationIn="fadeInRight" animationInDuration={1400} animationInDelay={100} isVisible={true}>
+          
+            <li className="fancynav-item fancy-dropdown"><a className="fancynav-link" href="/#"><span className="fancynav-link-content">Home </span></a>
+              <div className="fancy-dropdown-menu">
+                <div className="row pb-4 pt-3">
+                  <div className="col-auto">
+                    <a className="fancy-dropdown-item" href="home-default.html">Default</a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-          <li className="fancynav-item fancy-dropdown"><a className="fancynav-link" href="/#"><span className="fancynav-link-content">Portfolio </span></a>
-            <div className="fancy-dropdown-menu">
-              <div className="row pb-4 pt-3">
-                <div className="col-auto">
-                  <a className="fancy-dropdown-item" href="home-default.html">Default</a>
+            </li>
+            <li className="fancynav-item fancy-dropdown"><a className="fancynav-link" href="/#"><span className="fancynav-link-content">Portfolio </span></a>
+              <div className="fancy-dropdown-menu">
+                <div className="row pb-4 pt-3">
+                  <div className="col-auto">
+                    <a className="fancy-dropdown-item" href="home-default.html">Default</a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
+            </li>
+          
         </Animated>
-      )
-    }
-    return (
-      <div className="fancynavbar-addon"><a className="fancynavbar-addon-item" href="/#">yo</a></div>
-    )
+      ) } else {
+        return  (
+          <Animated animationIn="fadeOutRight" animationInDuration={1000} animationInDelay={0} isVisible={true}>
+            <li className="fancynav-item fancy-dropdown"><a className="fancynav-link" href="/#"><span className="fancynav-link-content">Home </span></a>
+              <div className="fancy-dropdown-menu">
+                <div className="row pb-4 pt-3">
+                  <div className="col-auto">
+                    <a className="fancy-dropdown-item" href="home-default.html">Default</a>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li className="fancynav-item fancy-dropdown"><a className="fancynav-link" href="/#"><span className="fancynav-link-content">Portfolio </span></a>
+              <div className="fancy-dropdown-menu">
+                <div className="row pb-4 pt-3">
+                  <div className="col-auto">
+                    <a className="fancy-dropdown-item" href="home-default.html">Default</a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </Animated>
+        )
+      }
+    
   }
 
   return (
@@ -51,7 +73,9 @@ const SideNav = () => {
         
         <div className={`fancynavbar${!isNavCollapsed ? '-not-collapse' : '-collapse'}`} id="navbarsExample09">
           <ul className="fancynavbar-nav">
+            
             <NavLinkAnimation />
+          
           </ul>
         </div>
       <div className="fancynavbar-addon">
