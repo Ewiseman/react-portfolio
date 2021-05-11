db6302b64c4e7e5d77e5660c356141eddff14e51
Eric Wiseman <ewiseman1.com>
2021-05-11 08:58:03 -0600

    got hamburger menu collapes working

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,167 +1,20 @@
-// import React, { useState, useRef } from "react";
-// import Hamburger from "hamburger-react";
-// import { Animated } from "react-animated-css";
-// import Blog from "./Blog.js";
-// import Hero from "./Hero.js";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-// const SideNav = () => {
-//   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
-
-//   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
-
-//   const NavLinkAnimation = (props) => {
-//     if (!isNavCollapsed) {
-//       return (
-//         <Router>
-//           <>
-//             <Animated
-//               animationIn="fadeInRight"
-//               animationInDuration={1400}
-//               animationInDelay={100}
-//               isVisible={true}
-//             >
-//               <li className="fancynav-item fancy-dropdown">
-//                 <a className="fancynav-link" href="/#languages">
-//                   <span
-//                     onClick={handleNavCollapse}
-//                     className="fancynav-link-content"
-//                   >
-//                     Home
-//                   </span>
-//                 </a>
-//               </li>
-//               <li className="fancynav-item fancy-dropdown">
-//                 <a className="fancynav-link" href="/">
-//                   <span className="fancynav-link-content">Work </span>
-//                 </a>
-//               </li>
-//               <li className="fancynav-item fancy-dropdown">
-//                 <a className="fancynav-link" href="/blog">
-//                   <span className="fancynav-link-content">Blog </span>
-//                 </a>
-//               </li>
-//               <li className="fancynav-item fancy-dropdown">
-//                 <a className="fancynav-link" href="/">
-//                   <span className="fancynav-link-content">Contact </span>
-//                 </a>
-//               </li>
-//             </Animated>
-//           </>
-//         </Router>
-//       );
-//     } else {
-//       return (
-//         <Animated
-//           animationIn="fadeOutRight"
-//           animationInDuration={1000}
-//           animationInDelay={0}
-//           isVisible={true}
-//         >
-//           <li className="fancynav-item fancy-dropdown">
-//             <span className="fancynav-link">
-//               <span className="fancynav-link-content">Home </span>
-//             </span>
-//           </li>
-//           <li className="fancynav-item fancy-dropdown">
-//             <span className="fancynav-link">
-//               <span className="fancynav-link-content">Work </span>
-//             </span>
-//           </li>
-//           <li className="fancynav-item fancy-dropdown">
-//             <a className="fancynav-link" href="/blog">
-//               <span className="fancynav-link-content">Blog </span>
-//             </a>
-//           </li>
-//           <li className="fancynav-item fancy-dropdown">
-//             <span className="fancynav-link">
-//               <span className="fancynav-link-content">Contact </span>
-//             </span>
-//           </li>
-//         </Animated>
-//       );
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <nav className="fancynavbar">
-//         <div className="fancynavbar-togglerbar">
-//           <h4>
-//             <a className="fancynavbar-brand" href="/#">
-//               EW
-//             </a>
-//           </h4>
-//           <button
-//             className="custom-toggler navbar-toggler"
-//             type="button"
-//             data-toggle="collapse"
-//             data-target="#navbarsExample09"
-//             aria-controls="navbarsExample09"
-//             aria-expanded={!isNavCollapsed ? true : false}
-//             aria-label="Toggle navigation"
-//             onClick={handleNavCollapse}
-//           >
-//             <div className="fancynavbar-toggler">
-//               <Hamburger color={"#fff"} />
-//             </div>
-//           </button>
-
-//           <div
-//             className={`fancynavbar${
-//               !isNavCollapsed ? "-not-collapse" : "-collapse"
-//             }`}
-//             id="navbarsExample09"
-//           >
-//             <ul className="fancynavbar-nav">
-//               <NavLinkAnimation />
-//             </ul>
-//           </div>
-//           <div className="fancynavbar-addon"></div>
-//         </div>
-//       </nav>
-//       <Router>
-//         <Switch>
-//           <Route exact path="/">
-//             <Hero />
-//           </Route>
-//           <Route exact path="/blog">
-//             <Blog />
-//           </Route>
-//         </Switch>
-//       </Router>
-//     </div>
-//   );
-// };
-
-// export default SideNav;
-
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Hero from "./Hero.js";
 import { Animated } from "react-animated-css";
 import Hamburger from "hamburger-react";
 
-// Each logical "route" has two components, one for
-// the sidebar and one for the main area. We want to
-// render both of them in different places when the
-// path matches the current URL.
-
-// We are going to use this route config in 2
-// spots: once for the sidebar and once in the main
-// content section. All routes are in the same
-// order they would appear in a <Switch>.
 const routes = [
   {
     path: "/",
     exact: true,
-    sidebar: () => <div>home!</div>,
+    sidebar: () => <div></div>,
     main: () => <Hero />,
   },
   {
-    path: "/bubblegum",
-    sidebar: () => <div>bubblegum!</div>,
-    main: () => <h2>Bubblegum</h2>,
+    path: "/blog",
+    sidebar: () => <div></div>,
+    main: () => <h2>blog</h2>,
   },
   {
     path: "/shoelaces",
@@ -175,43 +28,10 @@ export default function SideNav() {
 
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-  const NavLinkAnimation = (props) => {
-    if (!isNavCollapsed) {
-      return (
-        <Router>
-          <></>
-        </Router>
-      );
-    } else {
-      return (
-        <Animated
-          animationIn="fadeOutRight"
-          animationInDuration={1000}
-          animationInDelay={0}
-          isVisible={true}
-        >
-          <li className="fancynav-item fancy-dropdown">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="fancynav-item fancy-dropdown">
-            <span className="fancynav-link">
-              <span className="fancynav-link-content">Work </span>
-            </span>
-          </li>
-          <li className="fancynav-item fancy-dropdown">
-            <a className="fancynav-link" href="/blog">
-              <span className="fancynav-link-content">Blog </span>
-            </a>
-          </li>
-          <li className="fancynav-item fancy-dropdown">
-            <span className="fancynav-link">
-              <span className="fancynav-link-content">Contact </span>
-            </span>
-          </li>
-        </Animated>
-      );
-    }
-  };
+  const [isOpen, setOpen] = useState(false);
+
+  const menuItem = () => setOpen(!isOpen);
+
   return (
     <Router>
       <div>
@@ -225,15 +45,14 @@ export default function SideNav() {
             <button
               className="custom-toggler navbar-toggler"
               type="button"
-              data-toggle="collapse"
-              data-target="#navbarsExample09"
-              aria-controls="navbarsExample09"
-              aria-expanded={!isNavCollapsed ? true : false}
-              aria-label="Toggle navigation"
               onClick={handleNavCollapse}
             >
-              <div className="fancynavbar-toggler">
-                <Hamburger color={"#fff"} />
+              <div id="ham">
+                <Hamburger
+                  toggled={!isNavCollapsed}
+                  toggle={setIsNavCollapsed}
+                  color={"#fff"}
+                />
               </div>
             </button>
 
@@ -241,20 +60,24 @@ export default function SideNav() {
               className={`fancynavbar${
                 !isNavCollapsed ? "-not-collapse" : "-collapse"
               }`}
-              id="navbarsExample09"
             >
               <ul className="fancynavbar-nav">
                 {!isNavCollapsed && (
                   <Animated
                     animationIn="fadeInRight"
-                    animationInDuration={1400}
+                    animationInDuration={1000}
                     animationInDelay={100}
                     isVisible={true}
                   >
                     <Link to="/">
                       <li className="fancynav-item fancy-dropdown">
                         <span className="fancynav-link">
-                          <span className="fancynav-link-content">Home </span>
+                          <span
+                            className="fancynav-link-content"
+                            onClick={handleNavCollapse}
+                          >
+                            Home
+                          </span>
                         </span>
                       </li>
                     </Link>
@@ -262,49 +85,29 @@ export default function SideNav() {
                     <Link to="/blog">
                       <li className="fancynav-item fancy-dropdown">
                         <span className="fancynav-link">
-                          <span className="fancynav-link-content">Blog</span>
+                          <span
+                            className="fancynav-link-content"
+                            onClick={handleNavCollapse}
+                          >
+                            Blog
+                          </span>
                         </span>
                       </li>
                     </Link>
-                    <li className="fancynav-item fancy-dropdown">
-                      <a className="fancynav-link" href="/">
-                        <span className="fancynav-link-content">Contact </span>
-                      </a>
-                    </li>
-                  </Animated>
-                )}
-                :{" "}
-                {
-                  <Animated
-                    animationIn="fadeOutRight"
-                    animationInDuration={1000}
-                    animationInDelay={0}
-                    isVisible={true}
-                  >
-                    <Link to="/">
+                    <Link to="/contact">
                       <li className="fancynav-item fancy-dropdown">
-                        <a className="fancynav-link" href="/blog">
-                          <span className="fancynav-link-content">Blog </span>
-                        </a>
+                        <span className="fancynav-link">
+                          <span
+                            className="fancynav-link-content"
+                            onClick={handleNavCollapse}
+                          >
+                            Contact
+                          </span>
+                        </span>
                       </li>
                     </Link>
-                    <li className="fancynav-item fancy-dropdown">
-                      <span className="fancynav-link">
-                        <span className="fancynav-link-content">Work </span>
-                      </span>
-                    </li>
-                    <li className="fancynav-item fancy-dropdown">
-                      <a className="fancynav-link" href="/blog">
-                        <span className="fancynav-link-content">Blog </span>
-                      </a>
-                    </li>
-                    <li className="fancynav-item fancy-dropdown">
-                      <span className="fancynav-link">
-                        <span className="fancynav-link-content">Contact </span>
-                      </span>
-                    </li>
                   </Animated>
-                }
+                )}
               </ul>
             </div>
             <div className="fancynavbar-addon"></div>
